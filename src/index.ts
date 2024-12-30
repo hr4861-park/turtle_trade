928151e702376503b32e876e6d2c356d7c45513d
Hyangrae Park <hyangrae.park@gmail.com>
2024-12-30 21:38:09 +0900

    :bug: 시그널 반환 못하는 버그 수정

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ const main = () => {
   .then(() => telegram.registerCommand(/\/turtle (.+)/, async (msg, match) => {
     if (!match) return;
     const result = await indicator.readTurtleSignal(match[1])
-    if (!result) {
+    if (result) {
       await telegram.sendInfoMessage(`Current ${match[1]}'s turtle Signal: ${JSON.stringify(result)}`)
     }
   }))
