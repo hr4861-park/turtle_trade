451b8526d81cdf6e7b867c07a93a787957e5ac74
Hyangrae Park <hyangrae.park@gmail.com>
2025-01-02 19:39:09 +0900

    :sparkles: squeeze momentum 지표 값 추가

--- src/domain/TurtleSignal.ts
+++ src/domain/TurtleSignal.ts
@@ -6,8 +6,8 @@ export const parseTurtleSignal = typia.json.createAssertParse<TurtleSignal>()
 export interface TurtleSignal extends Omit<Candle, 'dateTime'> {
   high10: number,
   high20: number,
-  high55: number,
   low10: number,
   low20: number,
-  low55: number
+  value: number,
+  value_change: number
 }
