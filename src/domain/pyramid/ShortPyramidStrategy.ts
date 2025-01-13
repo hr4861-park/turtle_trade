18cda02fc541844f6dee525c10ff650c65895054
Hyangrae Park <53617557+hr4861-park@users.noreply.github.com>
2025-01-13 12:50:40 +0900

    ShortPyramidStrategy.ts 업데이트

--- src/domain/pyramid/ShortPyramidStrategy.ts
+++ src/domain/pyramid/ShortPyramidStrategy.ts
@@ -28,6 +28,6 @@ export class ShortPyramidStrategy implements PyramidStrategy {
   async run(): Promise<void> {
     await this.communicator.enterPosition(this.ticker, this.direction, this.size)
     await this.communicator.setStopLoss(this.ticker, this.direction, this.amount + this.size, this.currentPrice + this.atr * 2)
-    this.lastTradeRepository.upsert(this.ticker, this.direction, this.atr, this.size, this.currentPrice - this.atr)
+    await this.lastTradeRepository.upsert(this.ticker, this.direction, this.atr, this.size, this.currentPrice - this.atr)
   }
 }
