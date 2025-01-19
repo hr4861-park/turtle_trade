a567b2dc44d1b5dc6ecb7b0c90565831556c18a6
Hyangrae Park <hyangrae.park@gmail.com>
2025-01-19 21:05:51 +0900

    하프 클로즈 조건 추가

--- src/service/HaflCloseService.ts
+++ src/service/HaflCloseService.ts
@@ -8,19 +8,24 @@ import {BinanceCommunicator} from "../external/http/BinanceCommunicator";
 export class HalfCutService {
 
 
-  constructor(private readonly indicatorReader: IndicatorReader, private readonly positionReader: PositionReader,
+  constructor(private readonly indicatorReader: IndicatorReader,
+              private readonly positionReader: PositionReader,
               private readonly binance: BinanceCommunicator) {
   }
 
   run() {
     node_cron.schedule("5 9 * * *", async () => {
       const positions = await this.positionReader.getPositions()
+      const prices = await this.binance.fetchPrices()
       for (const ticker in positions) {
         const signal = await this.indicatorReader.readTurtleSignal(ticker)
+        const price = prices[ticker]
         if (!signal) {
           continue;
         }
-        await this.binance.halfClose(positions[ticker])
+        if (signal.value_change < 0 && positions[ticker].entryPrice < price) {
+          await this.binance.halfClose(positions[ticker])
+        }
       }
     })
   }
